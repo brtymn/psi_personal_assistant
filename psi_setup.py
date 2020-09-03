a97c11846c04e73c31b8975af28e30424598bede
Bartu <yaman.bartu@metu.edu.tr>
2020-09-03 22:58:39 +0300

    Added web browser features.

--- psi_setup.py
+++ psi_setup.py
@@ -1,7 +1,19 @@
 from gtts import gTTS
-from pygame import mixer
-import sys
+import re
 import time
+import webbrowser
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import smtplib
+import requests
+from pygame import mixer
+import urllib.request
+import urllib.parse
+import bs4
+import wikipedia
+import webbrowser
+
 
 from psi_greet import Greet
 
@@ -35,6 +47,23 @@ def reactPsi(command):
 
     if 'Hello' in command:
         talk('Hello! I am Psi. How can I help you?')
+
+    elif 'youtube' in command:
+        talk("Here is Youtube.\n")
+        webbrowser.open_new("https://www.youtube.com")
+
+    elif 'github' in command:
+        talk("Here is Github.\n")
+        webbrowser.open_new("https://github.com/brtymn")
+
+    elif 'metu mail' in command:
+        talk("Here is Metu Mail.\n")
+        webbrowser.open_new("https://horde.metu.edu.tr/login.php?url=https%3A%2F%2Fhorde.metu.edu.tr%2Fimp%2Fdynamic.php%3Fpage%3Dmailbox&horde_logout_token=WqeTi6ux-c2jmZutU68j0oR#mbox:SU5CT1g")
+
+    elif 'reddit' in command:
+        talk("Here is Reddit.\n")
+        webbrowser.open_new("https://www.reddit.com/")
+
     elif 'Exit' in command:
         talk("Psi deactivating now.")
         time.sleep(4)
